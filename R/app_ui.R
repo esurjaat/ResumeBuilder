8a6761938224a76cfd229aadb520339a82e91377
esurjaat <esurjaat@ucdavis.edu>
2022-03-24 17:38:00 -0700

    First draft of the app layout.

--- R/app_ui.R
+++ R/app_ui.R
@@ -9,8 +9,117 @@ app_ui <- function(request) {
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # Your application UI logic 
-    fluidPage(
-      h1("ResumeBuilder")
+    dashboardPage(
+      dashboardHeader(title = "Resume Builder"),
+      dashboardSidebar(
+        sidebarMenu(
+          menuItem("About", tabName = "about", icon = icon("info-sign", lib = "glyphicon")),
+          menuItem("Resume", tabName = "resume", icon = icon("th", lib = "glyphicon"),
+            menuSubItem("Inputs", tabName = "resume_inputs", icon = icon("pencil", lib = "glyphicon")),
+            menuSubItem("Build", tabName = "resume_build", icon = icon("list-alt", lib = "glyphicon")))
+          )),
+      dashboardBody(
+        tabItems(
+          tabItem(tabName = "about",
+                  includeMarkdown("R/About.md")),
+          tabItem(tabName = "resume_inputs",
+                  fluidRow(
+                    column(width = 3,
+                           downloadButton("template_empty", 
+                                          label = "Download Empty Template (.xlsx)")
+                           ),
+                    column(width = 3,
+                           downloadButton("template_filled",
+                                          label = "Download Survey as Template (.xlsx)"))
+                  ),
+                  br(),
+                  fluidRow(
+                    box(
+                      title = "Contact",
+                      width = 3,
+                      solidHeader = FALSE,
+                      status = "primary",
+                      mod_survey_inputs_contact_ui("survey_inputs_contact_1")
+                    ),
+                    box(
+                      title = "Education",
+                      width = 3,
+                      solidHeader = FALSE,
+                      status = "primary",
+                      mod_survey_inputs_education_ui("survey_inputs_education_1")
+                    ),
+                    column(
+                      width = 6,
+                      box(
+                        title = "Objective",
+                        width = NULL,
+                        solidHeader = FALSE,
+                        status = "primary",
+                        mod_survey_inputs_objective_ui("survey_inputs_objective_1")
+                      ),
+                      tabBox(
+                        title = "Skills",
+                        width = NULL,
+                        height = "272px",
+                        skills_tabPanel(id = "survey_inputs_experience",
+                                        number = 1),
+                        skills_tabPanel(id = "survey_inputs_experience",
+                                        number = 2),
+                        skills_tabPanel(id = "survey_inputs_experience",
+                                        number = 3),
+                        skills_tabPanel(id = "survey_inputs_experience",
+                                        number = 4),
+                        skills_tabPanel(id = "survey_inputs_experience",
+                                        number = 5),
+                        skills_tabPanel(id = "survey_inputs_experience",
+                                        number = 6),
+                      )
+                    ),
+                    tabBox(title = "Proffesional Experience",
+                           width = 12,
+                           experience_tabPanel(id = "survey_inputs_experience", 
+                                               number = 1),
+                           experience_tabPanel(id = "survey_inputs_experience", 
+                                               number = 2),
+                           experience_tabPanel(id = "survey_inputs_experience", 
+                                               number = 3),
+                           experience_tabPanel(id = "survey_inputs_experience", 
+                                               number = 4),
+                           experience_tabPanel(id = "survey_inputs_experience", 
+                                               number = 5),
+                           experience_tabPanel(id = "survey_inputs_experience", 
+                                               number = 6),
+                           )
+                  )),
+          tabItem(tabName = "resume_build",
+                  fluidRow(
+                    box(
+                      title = "Build Parameters",
+                      column(width = 6,
+                             radioButtons("build_from",
+                                          label = "Build From:",
+                                          choices = c("Template", "Survey"),
+                                          selected = "Template")),
+                      column(width = 6,
+                             conditionalPanel(
+                               condition = "input.build_from=='Template'",
+                               fileInput("upload_template",
+                                         label = "Upload Template"),
+                               actionButton("generate_template",
+                                            label = "Generate PDF")
+                             ),
+                             conditionalPanel(
+                               condition = "input.build_from=='Survey'",
+                               actionButton("generate_survey",
+                                            label = "Generate PDF")
+                             ))
+                      )
+                      
+                    )
+                  
+                  )
+        )
+      )
     )
   )
 }
