e57d19955e137e86d99035082560af3143726f98
esurjaat <esurjaat@ucdavis.edu>
2022-03-28 14:44:14 -0700

    textInput label changes

--- R/mod_survey_inputs_experience.R
+++ R/mod_survey_inputs_experience.R
@@ -13,14 +13,14 @@ mod_survey_inputs_experience_ui <- function(id){
     textInput(ns("title"), label = "Title", value = ""),
     textInput(ns("company"), label = "Company", value = ""),
     textInput(ns("city_state"), label = "City, State", value = ""),
-    dateInput(ns("date_start"), label = "date_start", value = Sys.Date()),
-    dateInput(ns("date_end"), label = "date_end", value = Sys.Date()),
-    textInput(ns("bullet1"), label = "Bullet Point 1", value = ""),
-    textInput(ns("bullet2"), label = "Bullet Point 2", value = ""),
-    textInput(ns("bullet3"), label = "Bullet Point 3", value = ""),
-    textInput(ns("bullet4"), label = "Bullet Point 4", value = ""),
-    textInput(ns("bullet5"), label = "Bullet Point 5", value = ""),
-    textInput(ns("bullet6"), label = "Bullet Point 6", value = "")
+    dateInput(ns("date_start"), label = "Start Date", value = NULL),
+    dateInput(ns("date_end"), label = "End Date", value = NULL),
+    textInput(ns("bullet1"), label = "Bullet Point 1 (Optional)", value = ""),
+    textInput(ns("bullet2"), label = "Bullet Point 2 (Optional)", value = ""),
+    textInput(ns("bullet3"), label = "Bullet Point 3 (Optional)", value = ""),
+    textInput(ns("bullet4"), label = "Bullet Point 4 (Optional)", value = ""),
+    textInput(ns("bullet5"), label = "Bullet Point 5 (Optional)", value = ""),
+    textInput(ns("bullet6"), label = "Bullet Point 6 (Optional)", value = "")
   )
 }
     
